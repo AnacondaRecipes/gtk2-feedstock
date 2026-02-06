c11e2fddbc524f8550e824631efb102430f286e7
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-02-06 20:26:43 +0200

    update patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* .
+cp "$BUILD_PREFIX"/share/gnuconfig/config.* .
 
 export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-}:${PREFIX}/lib/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig
@@ -14,8 +14,8 @@ elif [[ "${target_platform}" == linux-* ]]; then
     export LDFLAGS="${LDFLAGS} -Wl,-rpath=${PREFIX}/lib"
 fi
 
-export CFLAGS="${CFLAGS} -Wno-error=deprecated-declarations -Wno-error=cpp -Wno-error=pragmas -Wno-error=deprecated"
-export CXXFLAGS="${CXXFLAGS} -Wno-error=deprecated-declarations -Wno-error=cpp -Wno-error=pragmas -Wno-error=deprecated"
+export CFLAGS="${CFLAGS} -Wno-error=deprecated-declarations -Wno-error=cpp -Wno-error=pragmas -Wno-error=deprecated -Wno-incompatible-pointer-types"
+export CXXFLAGS="${CXXFLAGS} -Wno-error=deprecated-declarations -Wno-error=cpp -Wno-error=pragmas -Wno-error=deprecated -Wno-incompatible-pointer-types"
 
 ./configure \
   --prefix="${PREFIX}" \
@@ -28,5 +28,5 @@ export CXXFLAGS="${CXXFLAGS} -Wno-error=deprecated-declarations -Wno-error=cpp -
   --disable-gtk-doc \
   --with-html-dir="${SRC_DIR}/html"
 
-make V=1 -j$CPU_COUNT
-make install -j$CPU_COUNT
+make V=1 -j"$CPU_COUNT"
+make install -j"$CPU_COUNT"
