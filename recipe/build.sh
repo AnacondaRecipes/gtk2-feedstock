00b49b28869258cf7b28616d4d643ba76285faa9
vchychy <vchychyrko@anaconda.com>
2026-02-06 17:44:21 +0200

    fix flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -24,43 +24,10 @@ configure_args=(
     --with-html-dir="${SRC_DIR}/html"
 )
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-  unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
-  (
-    mkdir -p native-build
-    pushd native-build
-
-    export CC=$CC_FOR_BUILD
-    export AR=($CC_FOR_BUILD -print-prog-name=ar)
-    export NM=($CC_FOR_BUILD -print-prog-name=nm)
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-    export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
-
-    # Unset them as we're ok with builds that are either slow or non-portable
-    unset CFLAGS
-    unset CPPFLAGS
-    export host_alias=$build_alias
-    export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
-
-    ../configure --prefix=$BUILD_PREFIX "${configure_args[@]}"
-
-    # This script would generate the functions.txt and dump.xml and save them
-    # This is loaded in the native build. We assume that the functions exported
-    # by glib are the same for the native and cross builds
-    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
-    make -j${CPU_COUNT}
-    make install
-    popd
-  )
-  export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
-fi
-
-# ppc64le cdt need to be rebuilt with files in powerpc64le-conda-linux-gnu instead of powerpc64le-conda_cos7-linux-gnu. In the meantime:
-if [ "$(uname -m)" = "ppc64le" ]; then
-  cp --force --archive --update --link $BUILD_PREFIX/powerpc64le-conda_cos7-linux-gnu/. $BUILD_PREFIX/powerpc64le-conda-linux-gnu
-fi
-
+export CFLAGS="${CFLAGS} -Wno-incompatible-pointer-types -Wno-deprecated-declarations"
+export CXXFLAGS="${CXXFLAGS} -Wno-incompatible-pointer-types -Wno-deprecated-declarations"
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-}:${PREFIX}/lib/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig
+
 ./configure \
     --prefix="${PREFIX}" \
     "${configure_args[@]}"
